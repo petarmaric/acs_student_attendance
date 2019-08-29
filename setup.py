e291900ed3d4dc8fe50e219bc5250e417843c8c5
Petar Marić <petar.maric@gmail.com>
2019-08-29 14:22:57 +0200

    Update packaging info for 1.0.4 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='acs_student_attendance',
-    version='1.0.3',
+    version='1.0.4',
     url='https://github.com/petarmaric/acs_student_attendance',
     license='BSD',
     author='Petar Maric',
